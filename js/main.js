2e92a3de550868e401a822785cec9ae6218526a9
Maks Skomorokha <91227382+connars@users.noreply.github.com>
2023-01-24 06:40:21 +0200

    add burger animation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
 let header = document.querySelector('.header');
 let menu = document.querySelector('.burger');
+let lineOne = document.querySelector('.line1');
+let lineTwo = document.querySelector('.line2');
 
 function toggle() {
     header.classList.toggle('active');
-    console.log('click');
 }
 
-menu.addEventListener('click', toggle)
+function animate() {
+
+    if (header.matches('.active')){
+        lineOne.style.transform = "rotate(45deg)";
+        lineTwo.style.transform = "rotate(-45deg)";
+        lineOne.style.position = "absolute";
+        lineTwo.style.position = "absolute";
+        menu.style.gap = "0px";
+    } else {
+        lineOne.style.transform = "rotate(0deg)";
+        lineTwo.style.transform = "rotate(0deg)";
+        lineOne.style.position = "relative";
+        lineTwo.style.position = "relative";
+        menu.style.gap = "8px";
+    }
+
+}
+
+menu.addEventListener('click', toggle);
+menu.addEventListener('click', animate);
+
