259387a63b16ae74dcbb8c463777a057be0e4663
Maks Skomorokha <91227382+connars@users.noreply.github.com>
2023-01-25 19:30:10 +0200

    bug fix

--- js/main.js
+++ js/main.js
@@ -38,11 +38,35 @@ function openThemes() {
 
 picker.addEventListener('click', openThemes);
 
-let secondTheme = document.querySelector('.theme-two');
+let firstTheme = document.querySelector('.color-one');
+let secondTheme = document.querySelector('.color-two');
 
+const root = document.querySelector(':root');
 function changeTheme() {
+    if(firstTheme.matches('.current-theme')) {
+        firstTheme.classList.remove('current-theme');
+        root.style.setProperty('--green', 'rgb(147, 42, 207)');
+        root.style.setProperty('--bg', 'rgb(230, 217, 238)');
+        secondTheme.classList.add('current-theme');
+    } else {
 
-}
+    }
+};
 
-secondTheme.addEventListener('click', changeTheme)
+function changeTheme2() {
+    if(secondTheme.matches('.current-theme')) {
+        secondTheme.classList.remove('current-theme');
+        root.style.setProperty('--green', '#8BC34A');
+        root.style.setProperty('--bg', '#F9FCF6');
+        firstTheme.classList.add('current-theme');
+    }else {
+        
+    }
+};
+
+secondTheme.addEventListener('click', changeTheme);
+firstTheme.addEventListener('click', changeTheme2);
+
+// changeTheme()
+
 
