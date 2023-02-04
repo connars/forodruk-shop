6e8b2399aca759778298fe6132490b60ea446e04
Maks Skomorokha <91227382+connars@users.noreply.github.com>
2023-02-05 00:46:56 +0200

    add

--- js/cart.js
+++ js/cart.js
@@ -17,7 +17,56 @@ document.querySelector('.upload__sidebar-exit').addEventListener('click',() => {
     }
 });
 
+let accordionItem = document.querySelectorAll('.sidebar__accordion-item');
+let formInput = 1;
+let check = document.querySelectorAll('.check');
 
-document.querySelectorAll('.line').forEach( item => {
-    console.log(item);
+accordionItem[0].addEventListener('click', () => {
+    if(accordionItem[0].classList.contains('active')) {
+        accordionItem[0].classList.remove('active')
+        check[0].classList.add('active')
+    } else {
+        check[0].classList.remove('active')
+        accordionItem[0].classList.add('active')  
+        accordionItem[1].classList.remove('active')
+        accordionItem[2].classList.remove('active')
+    }
 })
+
+accordionItem[1].addEventListener('click', () => {
+    if(accordionItem[1].classList.contains('active')) {
+        accordionItem[1].classList.remove('active')
+    } else {
+        check[0].classList.add('active')
+        check[1].classList.remove('active')
+        accordionItem[0].classList.remove('active')
+        accordionItem[1].classList.add('active')
+        accordionItem[2].classList.remove('active')
+    }
+})
+
+accordionItem[2].addEventListener('click', () => {
+    if(accordionItem[2].classList.contains('active')) {
+        accordionItem[2].classList.remove('active');
+       
+    } else if(accordionItem[0].classList.contains('active')){
+        accordionItem[1].classList.add('active')
+        accordionItem[0].classList.remove('active')
+    } else if (formInput == '') {
+        alert('Заполните форму')
+    } else {
+        accordionItem[0].classList.remove('active')
+        accordionItem[1].classList.remove('active')
+        accordionItem[2].classList.add('active')
+        check[1].classList.add('active')
+    }
+})
+
+let imgUploader = document.querySelector('.openUploader');
+document.querySelectorAll('.upload-btn').forEach( uploadButton => {
+    uploadButton.addEventListener('click', () => {  
+        imgUploader.click();
+    })
+})
+
+
