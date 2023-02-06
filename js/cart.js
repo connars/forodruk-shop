3ae72d670bfce878a2da0bf9ca1c9ec420b39ddb
Maks Skomorokha <91227382+connars@users.noreply.github.com>
2023-02-06 10:05:01 +0200

    add

--- js/cart.js
+++ js/cart.js
@@ -18,46 +18,48 @@ document.querySelector('.upload__sidebar-exit').addEventListener('click',() => {
 });
 
 let accordionItem = document.querySelectorAll('.sidebar__accordion-title');
+let accordionBody = document.querySelectorAll('.sidebar__accordion-item');
+
 let formInput = 1;
 let check = document.querySelectorAll('.check');
 
 accordionItem[0].addEventListener('click', () => {
-    if(accordionItem[0].classList.contains('active')) {
-        accordionItem[0].classList.remove('active')
+    if(accordionBody[0].classList.contains('active')) {
+        accordionBody[0].classList.remove('active')
         check[0].classList.add('active')
     } else {
         check[0].classList.remove('active')
-        accordionItem[0].classList.add('active')  
-        accordionItem[1].classList.remove('active')
-        accordionItem[2].classList.remove('active')
+        accordionBody[0].classList.add('active')  
+        accordionBody[1].classList.remove('active')
+        accordionBody[2].classList.remove('active')
     }
 })
 
 accordionItem[1].addEventListener('click', () => {
-    if(accordionItem[1].classList.contains('active')) {
-        accordionItem[1].classList.remove('active')
+    if(accordionBody[1].classList.contains('active')) {
+        accordionBody[1].classList.remove('active')
     } else {
         check[0].classList.add('active')
         check[1].classList.remove('active')
-        accordionItem[0].classList.remove('active')
-        accordionItem[1].classList.add('active')
-        accordionItem[2].classList.remove('active')
+        accordionBody[0].classList.remove('active')
+        accordionBody[1].classList.add('active')
+        accordionBody[2].classList.remove('active')
     }
 })
 
 accordionItem[2].addEventListener('click', () => {
-    if(accordionItem[2].classList.contains('active')) {
-        accordionItem[2].classList.remove('active');
+    if(accordionBody[2].classList.contains('active')) {
+        accordionBody[2].classList.remove('active');
        
     } else if(accordionItem[0].classList.contains('active')){
-        accordionItem[1].classList.add('active')
-        accordionItem[0].classList.remove('active')
+        accordionBody[1].classList.add('active')
+        accordionBody[0].classList.remove('active')
     } else if (formInput == '') {
         alert('Заполните форму')
     } else {
-        accordionItem[0].classList.remove('active')
-        accordionItem[1].classList.remove('active')
-        accordionItem[2].classList.add('active')
+        accordionBody[0].classList.remove('active')
+        accordionBody[1].classList.remove('active')
+        accordionBody[2].classList.add('active')
         check[1].classList.add('active')
     }
 })
